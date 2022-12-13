be2e3240dc95b19647b8f0f60d8b02d64d5f3d70
Abdul <>
2022-12-13 11:08:27 +0400

    ci: next.config image domains add

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['images.microcms-assets.io', 'placehold.jp'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
